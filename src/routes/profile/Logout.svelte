15fba99879a877908e537745e3d7abddaa749fa4
Soare Catalin <stevensuncatalin369@gmail.com>
2022-11-06 09:24:31 +0200

    forgot to comment saving the token.

--- src/routes/profile/Logout.svelte
+++ src/routes/profile/Logout.svelte
@@ -1,12 +1,16 @@
 <script>
   import { onMount } from 'svelte'
   import { push } from 'svelte-spa-router'
+  import {info, token} from '../../stores'
 
 
   onMount(() => {
     localStorage.removeItem('info')
     localStorage.removeItem('phone')
     localStorage.removeItem('token')
+
+    $info = {}
+    $token = ""
 
     push('/welcome')
   })
